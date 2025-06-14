d6075678b7f06b55a0428b2d0fece5726d6244d3
azfaradhi <aradhihakim@gmail.com>
2025-06-14 16:36:08 +0700

    feat seaarch query

---
+++ src/controller/Controller.py
@@ -0,0 +1,82 @@
+import json.scanner
+from database.db_search import *
+from algorithms.KnuthMorris import *
+from algorithms.Levenshtein import *
+from algorithms.BoyerMoore import *
+from algorithms.AhoCorasick import *
+from utils.normalize_pdf import *
+import json
+
+
+class Controller:
+    def __init__(self,conn):
+        self.pdf = PDFTextConverter()
+        self.kmp = KnuthMorris()
+        self.booye = BoyerMoore()
+        self.aho = AhoCorasick()
+        self.search = db_search(conn)
+        self.allData = self.search.getAllData()
+        self.mapPathAndData = {}
+        self.preProcessText()
+        print(f"Total applicants: {len(self.allData)}")
+
+    def getDataByIndex(self, index: int):
+        if 0 <= index < len(self.allData):
+            return self.allData[index]
+        
+    def preProcessText(self):
+        for i, data in enumerate(self.allData):
+            if (i < 10):
+                path = data.get('cv_path', '')
+                if path not in self.mapPathAndData:
+                    self.pdf.set_pdf_path(path)
+                    text = self.pdf.to_text_normalized()
+                    self.mapPathAndData[path] = text
+                else:
+                    text = self.mapPathAndData[path]
+                print(f"Processing {i+1}/{len(self.allData)}: {path}")
+
+    def searchQuery(self, pattern: str, algorithm: str):
+        results = {}
+        for i, data in enumerate(self.allData):
+            if i < 10:
+                path = data.get('cv_path', '')
+                document_text = self.mapPathAndData.get(path, '')
+                if algorithm == 'kmp':
+                    self.kmp.set_text(document_text)
+                    foundedList = self.kmp.kmp_algorithm(pattern.lower())
+                elif algorithm == 'booye':
+                    self.booye.set_text(document_text)
+                    foundedList = self.booye.boyer_moore_algorithm(pattern.lower())
+                elif algorithm == 'aho':
+                    self.aho.set_text(document_text)
+                    self.aho.set_patterns(pattern.lower())
+                    foundedList = self.aho.search_patterns()
+                    
+                if foundedList:
+                    results[i] = {
+                        "applicant_id": data.get('applicant_id', ''),
+                        "count": len(foundedList)
+                    }
+
+        # Jika tidak ada hasil dari KMP, coba pakai Levenshtein
+        if not results:
+            print(f"Pattern '{pattern}' not found in any documents using KMP. Trying fuzzy matching...")
+            for i, data in enumerate(self.allData):
+                if i < 10:
+                    path = data.get('cv_path', '')
+                    document_text = self.mapPathAndData.get(path, '')
+                    self.kmp.set_text(document_text)
+                    foundedList = Levenshtein.search_levenhstein_from_long_string(pattern.lower(), document_text)
+
+                    if foundedList:
+                        results[i] = {
+                            "applicant_id": data.get('applicant_id', ''),
+                            "count": len(foundedList)
+                        }
+
+        return results
+    
+    def searchhh(self, text,pattern):
+        self.kmp.set_text(text)
+        return self.kmp.kmp_algorithm(pattern)
