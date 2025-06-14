0fbbf97dd9ace57b643c259f96c8166257e901eb
azfaradhi <aradhihakim@gmail.com>
2025-06-14 15:17:19 +0700

    fix utils

--- src/utils/normalize_pdf.py
+++ src/utils/normalize_pdf.py
@@ -2,6 +2,7 @@ import os
 import fitz
 import re
 
+
 class PDFTextConverter:
     def __init__(self):
         self.pdf_path = ""
@@ -11,6 +12,7 @@ class PDFTextConverter:
             raise FileNotFoundError(f"PDF file {pdf_path} does not exist.")
         self.pdf_path = pdf_path
 
+    # regex pake ini(?)
     def to_text_raw(self, pdf_path: str) -> str:
         self.set_pdf_path(pdf_path)
         doc = fitz.open(self.pdf_path)
@@ -21,6 +23,7 @@ class PDFTextConverter:
         doc.close()
         return full_text
 
+    #string matching pake ini yeah
     def to_text_normalized(self) -> str:
         doc = fitz.open(self.pdf_path)
         full_text = ""
@@ -33,3 +36,15 @@ class PDFTextConverter:
 
         full_text = ' '.join(full_text.split())
         return full_text
+
+# if __name__ == "__main__":
+#     converter = PDFTextConverter()
+#     pdf_file_path = "data/ACCOUNTANT/10554236.pdf"
+#     try:
+#         converter.set_pdf_path(pdf_file_path)
+#         text_raw = converter.to_text_normalized()
+#         print("Raw Text Extracted:")
+#         print(text_raw)
+
+#     except FileNotFoundError as e:
+#         print(e)
