49007ac7df403448100b82ef1bf3f191c246e6c6
Barru Adi <barru.adi@gmail.com>
2025-06-14 16:25:08 +0700

    feat: radio control

--- src/gui/pages/landing_page.py
+++ src/gui/pages/landing_page.py
@@ -1,15 +1,21 @@
 from PyQt5.QtWidgets import (
-    QWidget, QHBoxLayout, QScrollArea, QPushButton,
+    QWidget, QVBoxLayout, QPushButton,
 )
 
 class LandingPage(QWidget):
 
     def __init__(self, page_change):
         super().__init__()
-        layout = QHBoxLayout(self)
+        layout = QVBoxLayout(self)
 
         self.analyze_button = QPushButton("CV Analyzer")
         self.analyze_button.setMaximumWidth(200)
         self.analyze_button.setStyleSheet("padding: 10px; border-radius: 15px; background-color: #ccc;")
         self.analyze_button.clicked.connect(lambda: page_change(1))
         layout.addWidget(self.analyze_button)
+
+        self.about_button = QPushButton("About")
+        self.about_button.setMaximumWidth(200)
+        self.about_button.setStyleSheet("padding: 10px; border-radius: 15px; background-color: #ccc;")
+        self.about_button.clicked.connect(lambda: page_change(3))
+        layout.addWidget(self.about_button)
