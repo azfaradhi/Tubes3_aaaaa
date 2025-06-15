ce48b468ba02c5ad294af60620ad8bbec9c29075
azfaradhi <aradhihakim@gmail.com>
2025-06-15 21:10:12 +0700

    comment

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 
-from database.db_config import *
-from controller.Controller import Controller
-import json
-conn = connect()
-controller = Controller(conn)
-pattern = "technical"
-text = "aaaaaaaaa"
+# from src.database.db_config import *
+# from src.controller.Controller import Controller
+# from src.utils.normalize_pdf import PDFTextConverter
+# from src.algorithms.Regex import *
+# conn = connect()
+# controller = Controller(conn)
+# pattern = "technical"
+# text = "aaaaaaaaa"
 
-# print(controller.searchhh(pattern, text))
-a = controller.searchQuery("accounting")
 
-print(a)
+# pdf_converter = PDFTextConverter(max_workers=8)
+# pdf_converter.set_pdf_path("data/ACCOUNTANT/10554236.pdf")
+# text = pdf_converter.to_text_normalized()
+# print(text)
